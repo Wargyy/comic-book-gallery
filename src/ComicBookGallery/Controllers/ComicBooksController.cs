8bc964b9077f39a8d0ec19c87d353ea3458f4826
Diogo Miguel Freitas Cardoso <dmfcardoso@gmail.com>
2018-06-21 13:44:57 +0100

    Updated comic book view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,6 +11,18 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Peter Parker transforms in a <b>spider!</b></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Delgado",
+                "Inks: Some Guy",
+                "Colors: Bob Ross",
+                "Letters: Chris Words"
+            };
+
             return View();
         }
     }
